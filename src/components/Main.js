5a46bf14c0be9201dea5fc49cb3b078437e650dd
Naman <namanb454@gmail.com>
2023-08-01 22:39:40 +0530

    new commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,7 +56,7 @@ function Main() {
         </div>
         <h3 className='w-[40%] absolute lg:top-[75%] lg:left-[30%] left-auto top-auto transform -translate-x-1/2 -translate-y-1/2 z-10 text-white text-lg text-justify'>Empowering Your Business with Reliable IT Solutions for Uninterrupted Growth and Success. </h3>
         <div className='w-[50%] absolute lg:top-[53%] lg:left-[75%] left-auto top-auto transform -translate-x-1/2 -translate-y-1/2 z-10 text-white text-center  '>
-          <img src="home2.png" alt="Image" className={'w-full animate-pulse h-auto transition-transform'} />
+          <img src="home2.png" alt="Image" className={'w-full hover:animate-pulse h-auto transition-transform'} />
         </div>
       </div>
 
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -30,7 +30,7 @@ function Services() {
                                         {/* <CgWebsite className='w-10 h-16' /> */}
                                         {/* </div> */}
                                         {/* <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
-                                        <h1 class="title-font sm:text-2xl text-xl text-left font-bold text-white mb-3">{data.heading}</h1>
+                                        <h1 class="title-font sm:text-2xl text-xl text-left font-bold text-[#ec634b] mb-3">{data.heading}</h1>
                                         <p class="leading-relaxed text-left mb-3">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
                                         {/* <a class="text-indigo-500 inline-flex items-center">Learn More
                                             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
