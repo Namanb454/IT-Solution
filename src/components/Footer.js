3525741603a1dae133da0cf9dad30c96983b354a
Naman <namanb454@gmail.com>
2023-08-08 13:58:03 +0530

    new commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,64 +1,104 @@
-import React from 'react';
-import { AiFillInstagram, AiFillLinkedin, AiTwotoneMail, AiOutlinePhone, AiOutlineMail } from 'react-icons/ai';
+import React from 'react'
 
-const Footer = () => {
+function Footer() {
     return (
-        <footer className="text-white py-20 px-9">
-            <div className="container p-10 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1 sm:gap-4 md:gap-6">
-                {/* Intro */}
-                <div className="text-center sm:text-left">
-                    <h2 className="text-3xl font-normal mb-2 pb-3" style={{ fontFamily: 'Comic Sans MS' }}>Intro</h2>
-                    <p style={{ fontFamily: 'Bahnschrift' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-                {/* Useful Links */}
-                <div >
-                    <h2 className="text-3xl font-normal mb-2" style={{ fontFamily: 'Comic Sans MS' }}>Useful Links</h2>
-                    <ul className="list-disc list-inside">
-                        <li><a href="#" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Udemy</a></li>
-                        <li><a href="#" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>WikiPedia</a></li>
-                        <li><a href="#" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Google</a></li>
-                    </ul>
-                </div>
-
-                {/* Resources */}
-                <div>
-                    <h2 className="text-3xl font-]normal mb-2" style={{ fontFamily: 'Comic Sans MS' }}>Resources</h2>
-                    <ul className="list-disc list-inside">
-                        <li><a href="#" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Intro</a></li>
-                        <li><a href="#services" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Services</a></li>
-                        <li><a href="#features" style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Features</a></li>
-                    </ul>
-                </div>
-
-                {/* Contact Information */}
-                <div>
-                    <h2 className="text-3xl font-normal mb-2" style={{ fontFamily: 'Comic Sans MS' }}>Contact Information</h2>
-                    <div>
-                        <div className='flex py-3'>
-                            <AiOutlineMail style={{ fontSize: '25px' }} />
-                            <p className='px-3' style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Email: example@example.com</p>
-                        </div>
-                        <div className='flex py-3'>
-                            <AiOutlinePhone style={{ fontSize: '25px' }} />
-                            <p className='px-3' style={{ fontFamily: 'Bahnschrift', fontSize: '1.1rem' }}>Phone: (123) 456-7890</p>
-                        </div>
+        <div>
+            <footer class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+                    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+                        <a href='/' class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                            </svg>
+                            <span class="ml-3 text-xl font-bold text-white">B&K IT Solution</span>
+                        </a>
+                        {/* <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p> */}
                     </div>
-                    <div className="flex mt-2 py-2">
-                        <a href="https://www.instagram.com" target="_blank" className="mr-2 text-white hover:text-gray-400 px-1" >
-                            <i><AiFillInstagram style={{ fontSize: '30px' }} /></i>
-                        </a>
-                        <a href="https://www.linkedin.com" target="_blank" className="mr-2 text-white hover:text-gray-400 px-1">
-                            <i><AiFillLinkedin style={{ fontSize: '30px' }} /></i>
-                        </a>
-                        <a href="mailto:kartikkedia011@gmail.com" target="_blank" className="mr-2 text-white hover:text-gray-400 px-1">
-                            <i><AiTwotoneMail style={{ fontSize: '30px' }} /></i>
-                        </a>
+                    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+
+                        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-semibold text-[#ec634b] tracking-widest text-xl mb-3">USEFUL LINKS</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a href='#' class="text-white text-base hover:font-bold hover:text-[#9681EB]">Home</a>
+                                </li>
+                                <li>
+                                    <a href='#about' class="text-white text-base hover:font-bold hover:text-[#9681EB]">About Us</a>
+                                </li>
+                                <li>
+                                    <a href='#demo' class="text-white text-base hover:font-bold hover:text-[#9681EB]">Demo</a>
+                                </li>
+
+                            </nav>
+                        </div>
+                        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-semibold text-[#ec634b] tracking-widest text-xl mb-3">Support</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a href='#services' class="text-white text-base hover:font-bold hover:text-[#9681EB]">Services</a>
+                                </li>
+                                <li>
+                                    {/* <a class="text-gray-600 hover:text-[#9681EB]">Second Link</a> */}
+                                </li>
+                                <li>
+                                    <a href='#features' class="text-white text-base hover:font-bold hover:text-[#9681EB]">Features</a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div class="lg:w-1/3 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-semibold text-xl text-[#ec634b] tracking-widest mb-3">Contact Us</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a href='mailto: namanb454@gmail.com' class="text-white text-base hover:font-bold hover:text-[#9681EB]">namanb454@gmail.com</a>
+                                </li>
+                                <li>
+                                    <a href='mailto: kartikkedia011@gmail.com' class="text-white text-base hover:font-bold hover:text-[#9681EB]">kartikkedia011@gmail.com</a>
+                                </li>
+                                <li>
+                                    <a href='tel: 9599791185' class="text-white text-base hover:font-bold hover:text-[#9681EB]">+91 9599791185</a>
+                                </li>
+                                <li>
+                                    <a href='tel: 9599765990' class="text-white text-base hover:font-bold hover:text-[#9681EB]">+91 9599765990</a>
+                                </li>
+                            </nav>
+                        </div>
+
                     </div>
                 </div>
-            </div>
-        </footer>
-    );
-};
+                <div class="pb-10">
+                    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                        <p class="text-gray-500 text-sm font-semibold text-center sm:text-left">© 2023 B&K IT Solution
+                            {/* <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a> */}
+                        </p>
+                        <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                            <a class="text-gray-500">
+                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
 
-export default Footer;
+export default Footer
