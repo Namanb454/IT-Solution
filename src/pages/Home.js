daef0b298dd63a39f00b24c611c0336a8c5ad802
Naman <namanb454@gmail.com>
2023-10-07 14:32:35 +0530

    new commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-// import Typewriter from 'typewriter-effect';
 import Main from '../components/Main';
 import About from './About';
 import Services from './Services';
 import Features from './Features';
 import Footer from '../components/Footer';
 import ScrollButton from '../components/ScrollButton';
-import Pricing, { MeteorPreview } from './Pricing';
+import Pricing from './Pricing';
 import Projects from './Projects';
-// import { HeroScroll } from '../components/Code';
 
 
 function Home() {
